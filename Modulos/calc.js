497bd568ce4c6429b41ad64f99307cf5804d203d
= <=>
2019-04-26 21:51:21 -0300

    Installing Express.js

---
+++ Modulos/calc.js
@@ -0,0 +1,9 @@
+let sum = require('../sum');
+let sub = require('../sub');
+let multi = require('../multi');
+let div = require('../div');
+
+console.log(sum(1, 2));
+console.log(sub(4, 2));
+console.log(multi(3, 4));
+console.log(div(10, 2));
